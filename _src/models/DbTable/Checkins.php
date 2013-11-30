56445639344a7ba903c4751a8393ef08973b8dd1
Sandeep <narwalsandeep@gmail.com>
2013-11-30 20:40:30 +0000

    minor fix

---
+++ _src/models/DbTable/Checkins.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Model_DbTable_Checkins class.
+ * 
+ * @extends Core_Db
+ */
+class Model_DbTable_Checkins extends Core_Db{
+
+	/**
+	 * _name
+	 * 
+	 * (default value: 't5_checkins')
+	 * 
+	 * @var string
+	 * @access protected
+	 */
+	protected $_name = 't5_checkins';
+
+	
+	
+}
